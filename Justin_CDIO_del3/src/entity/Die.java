3447b33552fd08cc2d72b874ee9ef8a5c43c8bc1
Justin <jufa2401@gmail.com>
2016-11-24 16:48:49 +0100

    tilføjet .jar fil til projektmappen

--- Justin_CDIO_del3/src/entity/Die.java
+++ Justin_CDIO_del3/src/entity/Die.java
@@ -10,7 +10,7 @@ public class Die {
 
 	public int getValue(){
 		return FaceValue;   
-	}
+	}		
 }
 
 
--- Justin_CDIO_del3/src/entity/fieldclasses/Field.java
+++ Justin_CDIO_del3/src/entity/fieldclasses/Field.java
@@ -13,35 +13,37 @@ public abstract class Field {
 		this.ID = fieldIdentifier;
 		this.color = color;
 	}
-
-	public int getID() {
-		return ID;
-	}
-	
-	public Color getColor() {
-		return color;
-	}
-	
-/*
- * Defineres til at hente rent og price fra Fieldklassen. 
- * Det bliver smart når vi skal lave referencer i vores GUI-array
- * Alle underklasserne har denne metode med, selvom de ikke bruger dem
- * De vil returnere med 0, hvis de ikke skal bruge dem.
- * Definerer type til at genkende felternes type fra hinanden.
- */
-	public abstract int getRent();
-	public abstract int getPrice();
-	public abstract int getType();
-	
-
-	public abstract int landOnField(Player player);
-
 	public int[] getTax(int balance) {
 		int[] tax = {0,0}; 
 		return tax;
 	}
-}
 
+
+
+	public int getID() {
+		return ID;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	/*
+	 * Defineres til at hente rent og price fra Fieldklassen. 
+	 * Det bliver smart når vi skal lave referencer i vores GUI-array
+	 * Alle underklasserne har denne metode med, selvom de ikke bruger dem
+	 * De vil returnere med 0, hvis de ikke skal bruge dem.
+	 * Definerer type til at genkende felternes type fra hinanden.
+	 */
+	public abstract int getRent();
+	public abstract int getPrice();
+	public abstract int getType();
+
+
+	public abstract int landOnField(Player player);
+
+
+}
 
 
 
